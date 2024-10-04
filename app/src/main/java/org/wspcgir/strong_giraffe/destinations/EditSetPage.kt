8986cf42356b1c77994c7419fbe4766c1d046553
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2024-10-04 16:25:44 -0700

    Redesigns edit set page

--- app/src/main/java/org/wspcgir/strong_giraffe/destinations/EditSetPage.kt
+++ app/src/main/java/org/wspcgir/strong_giraffe/destinations/EditSetPage.kt
@@ -1,5 +1,7 @@
 package org.wspcgir.strong_giraffe.destinations
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import org.wspcgir.strong_giraffe.model.*
@@ -24,8 +27,10 @@ import org.wspcgir.strong_giraffe.repository.AppRepository
 import org.wspcgir.strong_giraffe.views.*
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import org.wspcgir.strong_giraffe.ui.theme.Grey
+import org.wspcgir.strong_giraffe.ui.theme.Red
+import org.wspcgir.strong_giraffe.ui.theme.StrongGiraffeTheme
 import java.time.Instant
 
 data class EditSetPageNavArgs(val id: SetId)
@@ -39,7 +44,7 @@ class EditSetPageViewModel(
     val locations: List<Location>,
     val exercises: List<Exercise>,
     val equipment: List<Equipment>,
-): ViewModel() {
+) : ViewModel() {
     fun submit() {
         viewModelScope.launch {
             repo.updateWorkoutSet(
@@ -62,17 +67,17 @@ class EditSetPageViewModel(
         viewModelScope.launch {
             val replacement = repo.latestSetForExerciseAtLocationExcluding(
                 inProgress.value.id,
-                inProgress.value.location,
+                location,
                 inProgress.value.exercise,
             )
             if (replacement != null) {
-               inProgressMut.value = inProgress.value.copy(
-                   location = replacement.location,
-                   equipment = replacement.equipment,
-                   exercise = replacement.exercise,
-                   reps = replacement.reps,
-                   weight = replacement.weight,
-               )
+                inProgressMut.value = inProgress.value.copy(
+                    location = replacement.location,
+                    equipment = replacement.equipment,
+                    exercise = replacement.exercise,
+                    reps = replacement.reps,
+                    weight = replacement.weight,
+                )
             } else {
                 inProgressMut.value = inProgress.value.copy(
                     location = location,
@@ -123,19 +128,19 @@ class EditSetPageViewModel(
         }
     }
 
-    fun changeReps(new: Reps){
+    fun changeReps(new: Reps) {
         inProgressMut.value = inProgress.value.copy(reps = new)
     }
 
-    fun changeWeight(new: Weight){
+    fun changeWeight(new: Weight) {
         inProgressMut.value = inProgress.value.copy(weight = new)
     }
 
-    fun changeIntensity(new: Intensity){
+    fun changeIntensity(new: Intensity) {
         inProgressMut.value = inProgress.value.copy(intensity = new)
     }
 
-    fun changeComment(new: Comment){
+    fun changeComment(new: Comment) {
         inProgressMut.value = inProgress.value.copy(comment = new)
     }
 
@@ -173,18 +178,20 @@ fun RegisterEditSetPage(
     }
 
     if (inProgress.value != null) {
-        EditSetPage(EditSetPageViewModel(
-            setId = navArgs.id,
-            inProgressMut = remember { mutableStateOf(inProgress.value!!) },
-            equipmentForLocationMut = remember {
-                mutableStateOf(equipment.filter { it.location == inProgress.value?.location })
-            },
-            repo = repo,
-            dest = dest,
-            locations = locations,
-            exercises = exercises,
-            equipment = equipment
-        ))
+        EditSetPage(
+            EditSetPageViewModel(
+                setId = navArgs.id,
+                inProgressMut = remember { mutableStateOf(inProgress.value!!) },
+                equipmentForLocationMut = remember {
+                    mutableStateOf(equipment.filter { it.location == inProgress.value?.location })
+                },
+                repo = repo,
+                dest = dest,
+                locations = locations,
+                exercises = exercises,
+                equipment = equipment
+            )
+        )
     }
 }
 
@@ -227,13 +234,13 @@ fun Page(
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     if (locations.isNotEmpty() && exercises.isNotEmpty()) {
-        var validReps by remember { mutableStateOf(true) }
-        var validWeight by remember { mutableStateOf(true) }
+        val validReps = remember { mutableStateOf(true) }
+        val validWeight = remember { mutableStateOf(true) }
 
         Scaffold(
             floatingActionButton = {
                 FloatingActionButton(onClick = submit) {
-                    if (validReps && validWeight) {
+                    if (validReps.value && validWeight.value) {
                         Icon(Icons.Default.Done, contentDescription = "Save Set")
                     } else {
                         Icon(Icons.Default.Warning, contentDescription = "Invalid fields")
@@ -245,85 +252,77 @@ fun Page(
             Column(
                 modifier = Modifier
                     .padding(innerPadding)
-                    .fillMaxSize(),
+                    .fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.spacedBy(10.dp),
             ) {
-                Text("Location")
-                LargeDropDownFromList(
-                    items = locations,
-                    label = "",
-                    itemToString = { it.name },
-                    onItemSelected = {
-                        if (starting.value.location != it.id) {
-                            changeLocation(it.id)
-                        }
-                    },
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    selectedIndex = locations.indexOfFirst { it.id == starting.value.location }
-                )
-                Text("Exercise")
-                LargeDropDownFromList(
-                    items = exercises,
-                    label = "",
-                    itemToString = { it.name },
-                    onItemSelected = { changeExercise(it.id) },
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    selectedIndex = exercises.indexOfFirst { it.id == starting.value.exercise }
-                )
-                Text("Equipment")
-                LargeDropDownFromList(
-                    items = equipment.value,
-                    label = "",
-                    itemToString = { it.name },
-                    onItemSelected = { changeEquipment(it.id) },
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    selectedIndex = equipment.value.indexOfFirst { it.id == starting.value.equipment }
-                )
-                Text("Reps")
-                IntField(
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    start = starting.value.reps.value
-                ) { it ->
-                    validReps = it != null
-                    if (it != null) {
-                        changeReps(Reps(it))
+                Spacer(Modifier.fillMaxHeight(0.1f))
+                Card() {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(10.dp),
+                        modifier = Modifier.padding(10.dp)
+                    ) {
+                        Text("Location")
+                        LargeDropDownFromList(
+                            items = locations,
+                            label = "",
+                            itemToString = { it.name },
+                            onItemSelected = {
+                                if (starting.value.location != it.id) {
+                                    changeLocation(it.id)
+                                }
+                            },
+                            modifier = Modifier.fillMaxWidth(0.8f),
+                            selectedIndex = locations.indexOfFirst { it.id == starting.value.location }
+                        )
+                        Text("Exercise")
+                        LargeDropDownFromList(
+                            items = exercises,
+                            label = "",
+                            itemToString = { it.name },
+                            onItemSelected = { changeExercise(it.id) },
+                            modifier = Modifier.fillMaxWidth(0.8f),
+                            selectedIndex = exercises.indexOfFirst { it.id == starting.value.exercise }
+                        )
+                        Text("Equipment")
+                        LargeDropDownFromList(
+                            items = equipment.value,
+                            label = "",
+                            itemToString = { it.name },
+                            onItemSelected = { changeEquipment(it.id) },
+                            modifier = Modifier.fillMaxWidth(0.8f),
+                            selectedIndex = equipment.value.indexOfFirst { it.id == starting.value.equipment }
+                        )
                     }
                 }
-                Text("Weight")
-                IntField(
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    start = starting.value.weight.value
-                ) { it ->
-                    validWeight = it != null
-                    if (it != null) {
-                        changeWeight(Weight(it))
+                Card() {
+                    RepsAndWeightSelector(
+                        starting,
+                        validReps,
+                        changeReps,
+                        validWeight,
+                        changeWeight
+                    )
+                }
+                Card() {
+                    IntensitySelector(changeIntensity, starting)
+                }
+                Card() {
+                    Column(
+                        modifier = Modifier.padding(10.dp)
+                    ){
+                        Text("Comment")
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(0.8f),
+                            value = starting.value.comment.value,
+                            onValueChange = { changeComment(Comment(it)) },
+                            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() })
+                        )
                     }
                 }
-                Text("Intensity")
-                LargeDropDownFromList(
-                    items = listOf(
-                        Intensity.NoActivation,
-                        Intensity.Easy,
-                        Intensity.Normal,
-                        Intensity.EarlyFailure,
-                        Intensity.Pain
-                    ),
-                    label = "",
-                    itemToString = Intensity::toString,
-                    onItemSelected = changeIntensity,
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    selectedIndex = Intensity.toInt(starting.value.intensity)
-                )
-                Text("Comment")
-                TextField(
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    value = starting.value.comment.value,
-                    onValueChange = { changeComment(Comment(it)) },
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                    keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() })
-                )
-                Spacer(modifier = Modifier.fillMaxHeight(0.1f))
+
                 Button(onClick = delete) {
                     Text("Delete")
                     Spacer(modifier = Modifier.fillMaxWidth(0.03f))
@@ -334,45 +333,143 @@ fun Page(
     }
 }
 
-@Preview
+@Composable
+private fun IntensitySelector(
+    changeIntensity: (Intensity) -> Unit,
+    starting: State<WorkoutSet>
+) {
+    var slider by remember(key1 = starting.value.intensity) {
+        mutableFloatStateOf(
+            Intensity.toInt(starting.value.intensity).toFloat()
+        )
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("${starting.value.intensity}")
+        Slider(
+            modifier = Modifier.fillMaxWidth(0.8f),
+            colors = SliderDefaults.colors(
+                thumbColor = intensityColor(starting.value.intensity),
+                activeTrackColor = intensityColor(starting.value.intensity)
+            ),
+            value = slider,
+            valueRange = 0f..4f,
+            steps = 3,
+            onValueChange = { f -> slider = f },
+            onValueChangeFinished = {
+                val result = Intensity.fromInt(slider.toInt())
+                if (result != null) {
+                    changeIntensity(result)
+                }
+                slider = slider.toInt().toFloat()
+            }
+        )
+    }
+}
+
+@Composable
+private fun RepsAndWeightSelector(
+    starting: State<WorkoutSet>,
+    validReps: MutableState<Boolean>,
+    changeReps: (Reps) -> Unit,
+    validWeight: MutableState<Boolean>,
+    changeWeight: (Weight) -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.fillMaxWidth(0.8f)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Reps")
+                IntField(
+                    start = starting.value.reps.value,
+                ) { it ->
+                    validReps.value = it != null
+                    if (it != null) {
+                        changeReps(Reps(it))
+                    }
+                }
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Weight")
+                IntField(
+                    start = starting.value.weight.value
+                ) { it ->
+                    validWeight.value = it != null
+                    if (it != null) {
+                        changeWeight(Weight(it))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Dark",
+    uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Preview(
+    name = "Light",
+    uiMode = UI_MODE_NIGHT_NO,
+    showBackground = true
+)
 @Composable
 private fun Preview() {
-    Page(
-        starting = remember { mutableStateOf(WorkoutSet(
-            id = SetId("a"),
-            exercise = ExerciseId("a"),
-            equipment = EquipmentId("equipA"),
-            location = LocationId("locationA"),
-            reps = Reps(0),
-            weight = Weight(0),
-            time = Instant.now(),
-            intensity = Intensity.Normal,
-            comment = Comment("")
-        ))},
-        submit = { },
-        delete = { },
-        changeLocation = { },
-        changeExercise = { },
-        changeEquipment = { },
-        changeReps = { },
-        changeWeight = { },
-        changeIntensity = { },
-        changeComment = { },
-        locations = listOf(
-            Location(LocationId("locationA"), "24 Hour Parkmoore"),
-            Location(LocationId("locationB"), "Fruitdale Apt. Gym"),
-        ),
-        equipment = remember {
-            mutableStateOf(
-                listOf(
-                    Equipment(EquipmentId("equipA"), "Barbell", LocationId("locationA")),
-                    Equipment(EquipmentId("equipB"), "Bicep Curl A", LocationId("locationA")),
+    StrongGiraffeTheme {
+
+        Page(
+            starting = remember {
+                mutableStateOf(
+                    WorkoutSet(
+                        id = SetId("a"),
+                        exercise = ExerciseId("a"),
+                        equipment = EquipmentId("equipA"),
+                        location = LocationId("locationA"),
+                        reps = Reps(0),
+                        weight = Weight(0),
+                        time = Instant.now(),
+                        intensity = Intensity.Normal,
+                        comment = Comment("")
+                    )
                 )
+            },
+            submit = { },
+            delete = { },
+            changeLocation = { },
+            changeExercise = { },
+            changeEquipment = { },
+            changeReps = { },
+            changeWeight = { },
+            changeIntensity = { },
+            changeComment = { },
+            locations = listOf(
+                Location(LocationId("locationA"), "24 Hour Parkmoore"),
+                Location(LocationId("locationB"), "Fruitdale Apt. Gym"),
+            ),
+            equipment = remember {
+                mutableStateOf(
+                    listOf(
+                        Equipment(EquipmentId("equipA"), "Barbell", LocationId("locationA")),
+                        Equipment(EquipmentId("equipB"), "Bicep Curl A", LocationId("locationA")),
+                    )
+                )
+            },
+            exercises = listOf(
+                Exercise(ExerciseId("a"), "Lat Pull Down", MuscleId("0")),
+                Exercise(ExerciseId("b"), "Squats", MuscleId("1")),
             )
-        },
-        exercises = listOf(
-            Exercise(ExerciseId("a"), "Lat Pull Down", MuscleId("0")),
-            Exercise(ExerciseId("b"), "Squats", MuscleId("1")),
         )
-    )
+    }
 }
