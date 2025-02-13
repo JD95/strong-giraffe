8714db2141e56444f10c997e222b3260d8ed70ac
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2025-02-13 15:51:15 -0800

    Adds exercise_variation table and test

--- app/src/androidTest/java/org/wspcgir/strong_giraffe/MigrationTests.kt
+++ app/src/androidTest/java/org/wspcgir/strong_giraffe/MigrationTests.kt
@@ -5,6 +5,7 @@ import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -41,5 +42,29 @@ class MigrationTests {
         ).addMigrations(*ALL_MIGRATIONS).build().apply {
             openHelper.writableDatabase.close()
         }
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun migrate_1_2() {
+        helper.createDatabase(TEST_DB, 1).apply {
+            execSQL( "INSERT INTO Muscle VALUES ('muscleA', 'bicep');")
+            execSQL( "INSERT INTO Exercise VALUES ('exerciseA', 'bicep curl', 'muscleA');")
+            execSQL("INSERT INTO Location VALUES ('locationA', 'gym');")
+            execSQL("INSERT INTO Equipment VALUES ('equipmentA', 'dumbbell', 'locationA');")
+            execSQL("INSERT INTO Equipment VALUES ('equipmentB', 'cables', 'locationA');")
+            execSQL(
+                """
+                INSERT INTO workout_set 
+                  VALUES ('setA', 'exerciseA', 'locationA', 'equipmentA', 0, 0, 123, 3, '1'), 
+                         ('setB', 'exerciseA', 'locationA', 'equipmentB', 1, 2, 345, 3, '2');
+                         ('setC', 'exerciseA', 'locationA', 'equipmentB', 2, 1, 543, 3, '3');
+                """.trimIndent())
+            close()
+        }
+
+        val db = helper.runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_1_2)
+        val cursor = db.query("SELECT * FROM exercise_variation")
+        assertEquals(cursor.count, 2)
     }
 }
--- app/src/main/java/org/wspcgir/strong_giraffe/repository/AppDatabase.kt
+++ app/src/main/java/org/wspcgir/strong_giraffe/repository/AppDatabase.kt
@@ -6,12 +6,13 @@ import androidx.room.migration.AutoMigrationSpec
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import org.wspcgir.strong_giraffe.repository.entity.*
+import java.util.UUID
 
 @Database(
     entities = [
         WorkoutSet::class, Location::class,
         Exercise::class, Muscle::class,
-        Equipment::class,
+        Equipment::class, ExerciseVariation::class
     ],
     views = [
         SetSummary::class
@@ -25,37 +26,84 @@ abstract class AppDatabase : RoomDatabase() {
 
 val MIGRATION_1_2 = object : Migration(1,2) {
     override fun migrate(database : SupportSQLiteDatabase) {
-        database.execSQL(
-            """
-            DROP INDEX index_workout_set_id;
-            DROP INDEX index_workout_set_location_exercise_equipment;
-            DROP INDEX index_workout_set_time;
-            """.trimIndent()
-        )
+        database.execSQL("DROP INDEX index_workout_set_id;");
+        database.execSQL("DROP INDEX index_workout_set_location_exercise_equipment;")
+        database.execSQL("DROP INDEX index_workout_set_time;")
+
         database.execSQL(
             """
             CREATE TABLE workout_set_new(
-              intensity INTEGER NOT NULL,
-              reps INTEGER NOT NULL,
-              exercise TEXT NOT NULL,
-              equipment TEXT NULL,
-              weight REAL NOT NULL,
-              location TEXT NULL,
-              comment TEXT NOT NULL,
               id TEXT NOT NULL,
+              exercise TEXT NOT NULL,
+              location TEXT NULL,
+              equipment TEXT NULL,
+              reps INTEGER NOT NULL,
+              weight REAL NOT NULL,
               time INTEGER NOT NULL,
+              intensity INTEGER NOT NULL,
+              comment TEXT NOT NULL,
               PRIMARY KEY(id),
               FOREIGN KEY(exercise) REFERENCES Exercise(id) ON DELETE CASCADE,
               FOREIGN KEY(location) REFERENCES Location(id) ON DELETE CASCADE 
             );
             """.trimIndent()
         )
+
         database.execSQL( "INSERT INTO workout_set_new SELECT * FROM workout_set;")
         database.execSQL("ALTER TABLE workout_set RENAME TO workout_set_old;")
         database.execSQL("ALTER TABLE workout_set_new RENAME TO workout_set;")
         database.execSQL("DROP TABLE workout_set_old;")
+
         database.execSQL("CREATE INDEX IF NOT EXISTS index_workout_set_id ON workout_set(id);")
         database.execSQL("CREATE INDEX IF NOT EXISTS index_workout_set_exercise ON workout_set(exercise);")
         database.execSQL("CREATE INDEX IF NOT EXISTS index_workout_set_time ON workout_set(time);")
+
+        database.execSQL(
+            """
+            CREATE TABLE exercise_variation(
+              id TEXT NOT NULL,
+              name TEXT NOT NULL,
+              exercise TEXT NOT NULL,
+              location TEXT NULL,
+              PRIMARY KEY(id),
+              FOREIGN KEY(exercise) REFERENCES Exercise(id) ON DELETE CASCADE,
+              FOREIGN KEY(location) REFERENCES Location(id) ON DELETE CASCADE 
+            );
+            """.trimIndent()
+        )
+        database.execSQL("CREATE INDEX IF NOT EXISTS index_exercise_variation_id ON exercise_variation(id);")
+        database.execSQL("CREATE INDEX IF NOT EXISTS index_exercise_variation_exercise ON exercise_variation(exercise);")
+
+        var exercises : Map<String, Set<(Pair<String, String>)>> = emptyMap()
+        val cursor = database.query(
+            """
+            SELECT
+              ws.exercise AS exercise, 
+              ws.location AS location, 
+              e.name AS name
+            FROM workout_set ws
+              JOIN Equipment e ON ws.equipment = e.id
+            """.trimIndent())
+        while (cursor.moveToNext()){
+            val exerciseId = cursor.getString(0)
+            val locationId = cursor.getString(1)
+            val equipmentName = cursor.getString(2)
+            val variants = exercises[exerciseId] ?: emptySet()
+            exercises = exercises.plus(exerciseId to variants.plus(Pair(equipmentName, locationId)))
+        }
+        for (pair in exercises) {
+            val exerciseId = pair.key
+            for (variation in pair.value) {
+                val id = UUID.randomUUID()
+                val name = variation.first
+                val location = variation.second
+                database.execSQL(
+                    """ 
+                    INSERT INTO exercise_variation 
+                    VALUES ('$id', '$name', '$exerciseId', '$location')
+                    """.trimIndent()
+                )
+            }
+        }
     }
 }
