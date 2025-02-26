91a1c23e41a358e4c5a2cf1ee6ce29c5d91c5f3c
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2025-02-26 09:37:02 -0800

    Removes unused import

--- app/src/main/java/org/wspcgir/strong_giraffe/destinations/EditSetPage.kt
+++ app/src/main/java/org/wspcgir/strong_giraffe/destinations/EditSetPage.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Warning
