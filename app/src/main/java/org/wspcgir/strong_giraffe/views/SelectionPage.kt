b2502ca6a0b4788962d9f3ea3dbd586272844d7b
JD95 <jeffreydwyer95@outlook.com>
2025-10-12 14:21:45 -0700

    Adds sorting and grouping on items

--- app/src/main/java/org/wspcgir/strong_giraffe/views/SelectionPage.kt
+++ app/src/main/java/org/wspcgir/strong_giraffe/views/SelectionPage.kt
@@ -1,56 +1,115 @@
 package org.wspcgir.strong_giraffe.views
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
-import androidx.compose.material3.Divider
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.wspcgir.strong_giraffe.ui.theme.StrongGiraffeTheme
 
+data class SortingOnGroup<T>(
+    val groupAndSort: (List<T>) -> Map<String, List<T>>,
+)
+
+fun <T> alphabeticOrdering(displayName: (T) -> String): SortingOnGroup<T> {
+    return SortingOnGroup { xs ->
+        xs.groupBy({ displayName(it).take(1).uppercase() }, { it })
+            .toSortedMap()
+            .mapValues { it.value.sortedBy { item -> displayName(item) } }
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SelectionPage() {
+fun <T> SelectionPage(
+    items: List<T>,
+    displayName: (T) -> String,
+    orderings: Map<String, SortingOnGroup<T>> = emptyMap()
+) {
+    var selectedOrdering: SortingOnGroup<T> by remember {
+        mutableStateOf(
+            orderings.firstNotNullOfOrNull { it.value }
+                ?: alphabeticOrdering(displayName)
+        )
+    }
+    val groups = selectedOrdering.groupAndSort(items)
     ModalDrawerScaffold(
         title = "Exercise",
-        drawerContent = { },
-        actionButton = { }
+        drawerContent = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OrderingDropdown(orderings) { ordering ->
+                    orderings[ordering]?.let { selectedOrdering = it }
+                }
+            }
+        },
+        actionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "navigate back")
+            }
+        }
     ) {
-        LazyColumn(
-            modifier = Modifier
-                .padding(10.dp)
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            val max = 20
-            for (i in 0..max) {
-                item() {
-                    Row (modifier = Modifier.fillMaxWidth(0.8f)) {
-                        ExerciseRow()
+        Column(modifier = Modifier.fillMaxHeight()) {
+            Spacer(modifier = Modifier.fillMaxHeight(0.1f))
+            LazyColumn(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                groups.forEach { (key, values) ->
+                    item {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(0.9f),
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(key, fontSize = 30.sp)
+                            }
+                        }
                     }
-                    if (i < max) {
-                        Divider(
-                            modifier = Modifier.fillMaxWidth(),
-                            color = Color.Black.copy(alpha = 0.10f)
-                        )
+                    values.forEach { value ->
+                        item {
+                            Row(modifier = Modifier.fillMaxWidth(0.9f)) {
+                                ExerciseRow(
+                                    title = displayName(value),
+                                    onSelect = {},
+                                    onEdit = {}
+                                )
+                            }
+                        }
                     }
                 }
             }
@@ -59,28 +118,108 @@ fun SelectionPage() {
 }
 
 @Composable
-fun ExerciseRow() {
-    Row (
+fun <T> OrderingDropdown(orderings: Map<String, SortingOnGroup<T>>, onSelect: (String) -> Unit) {
+    if (orderings.isNotEmpty()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text("Sort By", fontSize = 30.sp)
+        }
+        orderings.forEach { entry ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = { onSelect(entry.key) }) { Text(entry.key) }
+            }
+        }
+    } else {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text("No Sorting Available", fontSize = 30.sp)
+        }
+    }
+}
+
+@Composable
+fun ExerciseRow(
+    title: String,
+    onSelect: () -> Unit,
+    onEdit: () -> Unit
+) {
+    Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(5.dp)
+        horizontalArrangement = Arrangement.Absolute.Left,
+        modifier = Modifier.padding(2.dp)
     ) {
-        Column(modifier = Modifier.weight(0.9f)) {
-            Text("test", fontSize = 30.sp)
+        Column() {
+            IconButton(
+                onClick = onSelect,
+                modifier = Modifier.width(40.dp)
+            ) {
+                Icon(Icons.Outlined.CheckCircle, contentDescription = "Select ")
+            }
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Column(modifier = Modifier.weight(0.8f)) {
+            Text(title, fontSize = 20.sp)
         }
         Column(modifier = Modifier.weight(0.1f)) {
             IconButton(
-                onClick = {}
+                onClick = onEdit
             ) {
-                Icon(Icons.Filled.Build, contentDescription = "hello", modifier = Modifier.scale(1.0f))
+                Icon(
+                    Icons.Filled.Create,
+                    contentDescription = "edit button",
+                    modifier = Modifier.scale(1.0f)
+                )
             }
         }
     }
 }
 
 @Composable
-@Preview()
+@Preview(
+    name = "Dark",
+    uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true
+)
 private fun Preview() {
     StrongGiraffeTheme {
-        SelectionPage()
+        SelectionPage(
+            items = listOf(
+                "Crunches",
+                "Bicep Curls",
+                "Squats",
+                "Lat Pulldowns",
+                "Dips",
+                "Reverse Fly",
+                "Dead Lift",
+                "Tricep Extensions",
+                "Trap Shrugs",
+                "MR Life Fitness Black"
+            ),
+            displayName = { it },
+            orderings = mapOf(
+                "alphabetic" to alphabeticOrdering { it },
+                "rankings" to SortingOnGroup {
+                    it
+                        .groupBy(
+                            {
+                                if (listOf("Dead Lift", "Squats").contains(it)) {
+                                    "Awesome"
+                                } else {
+                                    "Lame"
+                                }
+                            },
+                            { name -> name }
+                        )
+                        .toSortedMap()
+                        .mapValues { entry -> entry.value.sortedBy { name -> name } }
+                },
+            )
+        )
     }
 }
